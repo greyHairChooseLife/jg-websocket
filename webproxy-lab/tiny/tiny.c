de89ddc7345a2c8ef3f1c73cc0acf449d3bd2ddc
sangyeon kim <greyhairchooselife@gmail.com>
2025-11-04 10:48:57 +0900

    doit: 에러 반환 포멧 함수 및 !GET method 거르기

--- webproxy-lab/tiny/tiny.c
+++ webproxy-lab/tiny/tiny.c
@@ -37,12 +37,57 @@ int main(int argc, char** argv) {
     while (1)
     {
         clientlen = sizeof(clientaddr);
-        connfd = Accept(listenfd, (SA*)&clientaddr,
-                        &clientlen);  // line:netp:tiny:accept
+        connfd = Accept(listenfd, (SA*)&clientaddr, &clientlen);
         Getnameinfo((SA*)&clientaddr, clientlen, hostname, MAXLINE, port,
                     MAXLINE, 0);
         printf("Accepted connection from (%s, %s)\n", hostname, port);
-        doit(connfd);   // line:netp:tiny:doit
-        Close(connfd);  // line:netp:tiny:close
+        doit(connfd);
+        Close(connfd);
     }
+}
+
+void doit(int connfd) {
+    rio_t rp;
+    char rawReq[MAXBUF];
+    char method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+
+    rio_readinitb(&rp, connfd);
+    rio_readlineb(&rp, rawReq, MAXBUF);
+    printf("req in raw ----------> %s", rawReq);
+
+    sscanf(rawReq, "%s %s %s", method, uri, version);  // GET / HTTP/1.1
+    printf("req in [%s %s %s]\n", method, uri, version);
+
+    if (strcasecmp(method, "GET"))  // returns not "\0" when it's different
+    {
+        clienterror(connfd, method, "501", "GET method only",
+                    "Server does not serve such thing.");
+        return;
+    }
+}
+
+void clienterror(int fd,
+                 char* cause,
+                 char* errnum,
+                 char* shortmsg,
+                 char* longmsg) {
+    char header[MAXLINE], body[MAXBUF];
+
+    /* Build the HTTP response body */
+    sprintf(body, "<html><title>Tiny Error</title>");
+    sprintf(body,
+            "%s<body style=\"background-color: #000000; color: #FFFFFF\">\r\n",
+            body);
+    sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+    sprintf(body, "%s<p>%s: %s</p>\r\n", body, longmsg, cause);
+    sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
+    sprintf(body, "%s</body></html>\r\n", body);
+
+    /* Print the HTTP response */
+    sprintf(header, "HTTP/1.1 %s %s\r\n", errnum, shortmsg);
+    sprintf(header, "%sContent-type: text/html\r\n", header);
+    sprintf(header, "%sContent-length: %d\r\n\r\n", header, (int)strlen(body));
+
+    Rio_writen(fd, header, strlen(header));
+    Rio_writen(fd, body, strlen(body));
 }
